0eae5f26ef2d2f32abb0c15495156dfc07b52438
Gabi170 <107480290+Gabi170@users.noreply.github.com>
2022-07-14 16:18:41 -0300

    Add files via upload

---
+++ Daseafio de Typescript/Desafio2/Atualizar tsconfig.ts
@@ -0,0 +1,59 @@
+/*Como podemos melhorar o esse código usando TS? 
+
+let pessoa1 = {};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2 = {}
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3 = {
+    nome: "laura",
+    idade: "32",
+    profissao: "Atriz"
+};
+
+let pessoa4 = {
+    nome = "carlos",
+    idade = 19,
+    profissao = "padeiro"
+}*/
+
+enum emprego{atriz, padeiro}
+
+type humano={nome:string, idade:number, profissao:emprego}
+
+let pessoa1:humano = {
+
+    nome:'Maria',
+    idade: 29,
+    profissao: emprego.atriz,
+
+}
+
+let pessoa2:humano = {
+
+    nome: 'Roberto',
+    idade: 19,
+    profissao: emprego.padeiro, 
+
+}
+
+let pessoa3:humano = {
+
+    nome: 'Laura',
+    idade: 32,
+    profissao: emprego.atriz,
+
+} 
+
+let pessoa4:humano = {
+
+    nome: 'Carlos',
+    idade: 19,
+    profissao : emprego.padeiro,
+
+}
